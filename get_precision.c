d99b236ef28dcb478b0b251a80acbef5155ea90a
Your Name <you@example.com>
2023-03-27 14:21:22 -0700

    project

--- get_precision.c
+++ get_precision.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
@@ -7,29 +8,41 @@
  *
  * Return: Precision.
  */
-int get_precision(const char *format, int *i, va_list list)
-{
-int curr_i = *i + 1;
-int precision = -1;
-if (format[curr_i] != '.')
-return (precision);
-precision = 0;
-for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-{
-if (is_digit(format[curr_i]))
-{
-precision *= 10;
-precision += format[curr_i] - '0';
-}
-else if (format[curr_i] == '*')
-{
-curr_i++;
-precision = va_arg(list, int);
-break;
-}
-else
-break;
-}
-*i = curr_i - 1;
-return (precision);
+int get_precision(const char *format, int i, ...) {
+    va_list args;
+    va_start(args, i);
+
+    // find the position of the ith argument in the argument list
+    int arg_pos = 1;
+    while (*format && arg_pos < i) {
+        if (*format == '%') {
+            // skip over any flags, width, and length modifiers
+            format++;
+            while (*format && !strchr("diouxXfFeEgGaAcspn%", *format))
+                format++;
+            if (*format == '\0')
+                break;
+            if (*format == '%' || *format == 'n') {
+                // ignore these conversion specifiers
+            } else {
+                arg_pos++;
+            }
+        } else {
+            format++;
+        }
+    }
+
+    // find the precision specifier (if any) for the ith argument
+    int precision = -1;
+    while (*format && strchr(".0123456789", *format)) {
+        if (*format == '.') {
+            precision = 0;
+        } else if (precision >= 0) {
+            precision = precision * 10 + (*format - '0');
+        }
+        format++;
+    }
+
+    va_end(args);
+    return precision;
 }
